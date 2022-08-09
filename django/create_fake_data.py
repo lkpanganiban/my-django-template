4df2b0e2c4e207d547659b2683b7e63706d4a043
Ian Panganiban <lkp@noypimaps.com>
2022-08-10 07:11:35 +0800

    chore: updated faker generation values

--- django/create_fake_data.py
+++ django/create_fake_data.py
@@ -1,40 +1,37 @@
 import os 
 from random import randint
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
-
 import django 
-django.setup() 
 
-from faker import factory,Faker 
+from faker import Faker
 from apps.core.files.models import * 
 from apps.core.users.models import *
 from django.contrib.auth.models import Group
 from model_bakery.recipe import Recipe,foreign_key
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+django.setup() 
+
+
 fake = Faker() 
 
 for k in range(20):
-    try:
-        first_name, last_name = fake.name().split(" ")
-    except:
-        continue
     email = fake.email()
     user = Recipe(User,
-                  first_name = first_name,
-                  last_name = last_name,
+                  first_name = fake.first_name,
+                  last_name = fake.last_name(),
                   email = email,
                   username= email
     )
     file_set = Recipe(FileSet,
             owner=foreign_key(user)
     )
-    filename = fake.company()
+    filename = fake.file_name()
     files = Recipe(Files,
             name=filename,
             file_set=foreign_key(file_set),
-            file_size=randint(100,2000),
+            file_size=randint(100,10000),
             description=fake.sentence(),
-            file_type="pdf",
-            location=fake.sentence()
+            file_type=filename.split(".")[1],
+            location=fake.file_path
     )
     files.make()
