48a01783a260d2af0162e096a992b01e0069fe37
Ian Panganiban <lkp@noypimaps.com>
2022-08-11 20:06:06 +0800

    chore: added error logging across endpoints

--- django/apps/core/users/api.py
+++ django/apps/core/users/api.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions, viewsets
+from rest_framework_tracking.mixins import LoggingMixin
 
 from .serializers import RegisterSerializer, UserSerializer
 
 
-class RegisterView(generics.CreateAPIView):
+class RegisterView(LoggingMixin, generics.CreateAPIView):
     queryset = User.objects.none()
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
 
+    def should_log(self, request, response):
+        """Log only errors"""
+        return response.status_code >= 400
 
-class UserViewset(viewsets.ReadOnlyModelViewSet):
+
+class UserViewset(LoggingMixin, viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     permission_classes = (permissions.IsAdminUser,)
     serializer_class = UserSerializer
+
+    def should_log(self, request, response):
+        """Log only errors"""
+        return response.status_code >= 400
