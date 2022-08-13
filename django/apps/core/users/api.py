9a57e61f663b8b149cbc096ebcfa14c26dccd520
Ian Panganiban <lkp@noypimaps.com>
2022-08-13 19:30:09 +0800

    feat: exposed profile endpoint (#11)

--- django/apps/core/users/api.py
+++ django/apps/core/users/api.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions, viewsets
 from rest_framework_tracking.mixins import LoggingMixin
-
-from .serializers import RegisterSerializer, UserSerializer
+from .models import Profile
+from .serializers import RegisterSerializer, UserSerializer, ProfileSerializer
 
 
 class RegisterView(LoggingMixin, generics.CreateAPIView):
@@ -23,3 +23,15 @@ class UserViewset(LoggingMixin, viewsets.ReadOnlyModelViewSet):
     def should_log(self, request, response):
         """Log only errors"""
         return response.status_code >= 400
+
+class ProfileViewset(LoggingMixin, viewsets.ReadOnlyModelViewSet):
+    queryset = Profile.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = ProfileSerializer
+
+    def should_log(self, request, response):
+        """Log only errors"""
+        return response.status_code >= 400
+    
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
