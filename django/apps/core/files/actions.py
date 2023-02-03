cc6f575495f7b1b7fff3fb290e9fde39c8f63ff0
Ian Panganiban <ian.panganiban@outlook.ph>
2023-02-04 07:12:45 +0800

    added docstrings to actions.py;

--- django/apps/core/files/actions.py
+++ django/apps/core/files/actions.py
@@ -5,10 +5,13 @@ from guardian.shortcuts import (
     get_users_with_perms,
 )
 
-from .models import FileSet, Files, Subscriptions
+from .models import FileSet, Files, Subscriptions, User
 
 
 def _move_files_set(fs_main: FileSet, fs_sub: FileSet) -> bool:
+    """
+    move files from one set to another
+    """
     for f in Files.objects.filter(file_set__in=fs_sub):
         f.file_set = fs_main
         f.save()
@@ -29,7 +32,12 @@ def merge_sets(set_list: list) -> str:
     return f"merged {sub_sets_uuid} with {main_set_uuid}"
 
 
-def move_file_set_to_another_subscription(subscription: Subscriptions, fs_id: str) -> FileSet:
+def move_file_set_to_another_subscription(
+    subscription: Subscriptions, fs_id: str
+) -> FileSet:
+    """
+    move fileset from one subscription to another
+    """
     fs = FileSet.objects.get(id=fs_id)
     fs.subscription = subscription
     fs.save()
@@ -37,29 +45,45 @@ def move_file_set_to_another_subscription(subscription: Subscriptions, fs_id: st
 
 
 def assign_moderator_permissions(user, fs_id: str, fs: FileSet = None) -> bool:
+    """
+    assign user moderator permissions to a fileset
+    """
     if fs is None:
         fs = FileSet.objects.get(id=fs_id)
     assign_perm("can_moderate_files", user, fs)
     return fs.has_moderator_access(user)
 
 
-def remove_moderator_permissions(user, fs_id: str, fs: FileSet = None) -> bool:
-    if fs is None:  # if fileset is empty default using fs_id
+def remove_moderator_permissions(user: User, fs_id: str, fs: FileSet = None) -> bool:
+    """
+    remove user moderator permissions from a fileset
+    """
+    # if fileset is None default using fs_id
+    if fs is None:
         fs = FileSet.objects.get(id=fs_id)
     remove_perm("can_moderate_files", user, fs)
     return fs.has_moderator_access(user)
 
 
 def fetch_fileset_moderator_permissions(user) -> FileSet:
+    """
+    retrieves users which has moderator permissions for a fileset
+    """
     return get_objects_for_user(user, "can_moderate_files", FileSet.objects.all())
 
 
 def add_moderator_permission_to_moderators(fs: FileSet) -> None:
+    """
+    apply moderator permissions to a fileset
+    """
     for moderator in fs.moderators.all():
         assign_moderator_permissions(moderator, str(fs.id))
 
 
 def reset_moderator_permissions(fs) -> None:
+    """
+    remove or reset permissions for a fileset
+    """
     moderators = fs.moderators.all()
     users_with_perm = get_users_with_perms(fs)
     # remove users
--- django/apps/core/users/actions.py
+++ django/apps/core/users/actions.py
@@ -5,6 +5,9 @@ from .models import User
 def _send_email_registrations(
     registration_subject:str, text_content:str, html_content:str, from_email:str, email_list:list, bcc_email:str
 ) -> None:
+    """
+    send email registration notification
+    """
     if not settings.EMAIL_SEND: return "mocking email sending"
     msg = EmailMultiAlternatives(
         registration_subject, text_content, from_email, email_list, bcc=bcc_email
@@ -14,9 +17,15 @@ def _send_email_registrations(
 
 
 def _check_user_if_in_platform(email: str) -> bool:
+    """
+    checks if user exists
+    """
     return User.objects.filter(email=email).exists()
 
 def invite_user_to_subscription(email: str) -> bool:
+    """
+    invite user to be added to a subscription
+    """
     if not _check_user_if_in_platform(email):
         print("adding user to platform")
     if settings.EMAIL_SEND:
